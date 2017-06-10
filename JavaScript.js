587ff7867ce59be6fafaef69652c6bc56f1e8085
Ahmad <ahmad.hussain.307@gmail.com>
2017-06-10 13:32:27 +0200

    bug fix-sharing to tumblr

--- JavaScript.js
+++ JavaScript.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     
     $("#btn").on("click", function () {
         $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-            $(".theQ").html(a[0].content + "<p>— " + a[0].title + "</p>")
+            $(".theQ").html(a[0].content + "<p>— " + a[0].title + "</p>");
             quote+=$(".theQ").text();
             right=true;
         });
@@ -18,6 +18,7 @@ $(document).ready(function () {
     });
     
     $("#tumblr").on("click",function(){
+        if (right===false) quote+="All great achievements require time. -Maya Angelou";
         $("#tumblr").attr("href",'https://www.tumblr.com/widgets/share/tool?caption='+'"'+quote+'"'+' &canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
     });
 });
